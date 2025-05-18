adfb2668e503541dafc6ab452f711791a16e8d07
jakariachynabil10 <jakariachynabil@gmail.com>
2025-05-18 20:02:15 +0600

    slug added

---
+++ src/app/Pets/[...slug]/page.tsx
@@ -0,0 +1,11 @@
+
+
+const CatchAllRoutes = () => {
+    return (
+        <div>
+            <h1>Catch All Routes</h1>
+        </div>
+    );
+};
+
+export default CatchAllRoutes;
--- src/app/Pets/[PetId]/page.tsx
+++ src/app/Pets/[PetId]/page.tsx
@@ -1,13 +1,16 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client"
-import { useParams } from "next/navigation";
+import { useParams, useSearchParams } from "next/navigation";
 
 const DynamicPage = () => {
+  
     const params = useParams()
+    const searchParams = useSearchParams()
+    console.log(searchParams)
     
   return (
     <div>
-      <h1>Dynamic Pet Page {params.PetId}</h1>
+      <h1>Dynamic Pet Page {params.petId}</h1>
     </div>
   );
 };
